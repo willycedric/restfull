434a5d88ca8fe69e7d929c439abf846827e88450
Willy DONGMO <w.dongmo@gmail.com>
2019-08-21 23:23:29 +0200

    Adding http and https support

---
+++ config.js
@@ -0,0 +1,33 @@
+/** 
+ * Create and export configuration variables
+ */
+
+
+ // Container for all the environments
+var environments = {}
+
+// Staging (default) environment
+
+environments.staging = {
+ 
+    'httpPort':3000,
+    'httpsPort':3001,
+    'envName':'staging'
+   
+}
+
+// Production environment
+environments.production = {
+
+    'httpPort':5000,
+    'httpsPort':5001,
+    'envName': 'production'
+}
+
+//Determine wich environment was passed as a command-line argument
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string'?process.env.NODE_ENV.toLowerCase():''
+
+//Check that the current environment is one of the environments above, if not, defaut to staging
+var environmentToExport = typeof(environments[currentEnvironment]) == 'object'? environments[currentEnvironment]:environments.staging
+
+module.exports = environmentToExport
--- index.js
+++ index.js
@@ -2,31 +2,113 @@
  * Primary file the the API
  */
 
- //Dependencies
+//Dependencies
 
- var http = require('http')
+var http = require('http')
+var https = require('https')
 var url = require('url')
- //The server should respond to all request with a string
+var StringDecoder = require('string_decoder').StringDecoder
+var config = require('./config')
+var fs = require('fs')
+//The server should respond to all request with a string
 
-var server = http.createServer(function(req, res){
-    // Get the URL and parse it
-    var parsedUrl = url.parse(req.url, true)
-    
-    //Get the path 
-    //get the untrimmed path
-    var path = parsedUrl.pathname
-    var trimmedPath = path.replace(/^\/+|\/+$/g,'')
-   
-    //Send the response
-    res.end('Hello world\n')
 
-    //Log the request path 
+// Instantiate the HTTP server
+var httpServer = http.createServer(function (req, res) {
+    unifiedServer(req, res)
+})
 
-  
+//Start the server, and have it listen on port 3000
+httpServer.listen(config.httpPort, function () {
+    console.log(`The server is listening on port ${config.httpPort}  now,on ${config.envName} mode`)
 })
 
 
- //Start the server, and have it listen on port 3000
- server.listen(3000, function(){
-     console.log("The server is listening on port 3000 now")
- })
+// Instantiate the HTTPS Server
+var  httpsServerOptions = {
+    'key': fs.readFileSync('./https/key.pem') ,
+    'cert':fs.readFileSync('./https/cert.pem')
+}
+var httpsServer = https.createServer(httpsServerOptions,function (req, res) {
+    unifiedServer(req, res)
+})
+
+//start the https server
+httpsServer.listen(config.httpsPort, function () {
+    console.log(`The server is listening on port ${config.httpsPort}  now,on ${config.envName} mode`)
+})
+
+
+
+
+
+// All the server logic for both http and https server
+var unifiedServer = function (req, res) {
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url, true)
+
+    //Get the path 
+    //get the untrimmed path
+    var path = parsedUrl.pathname
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '')
+    //get the query string as an object 
+    var queryStringObject = parsedUrl.query
+    //get the http method
+    var method = req.method.toLowerCase()
+    //Get the headers as an object
+    var headers = req.headers
+    //parsing the payload, if any
+    var decoder = new StringDecoder('utf-8')
+    var buffer = ''
+    req.on('data', function (data) {
+        buffer += decoder.write(data)
+    })
+
+    req.on('end', function () {
+        buffer += decoder.end()
+        //Route the request to appropriate handler on the notFound otherwise
+        var chosenHandler = typeof (router[trimmedPath]) != 'undefined' ? router[trimmedPath] : handlers.notFound
+        //construct the data object to send to the handler
+        var data = {
+            trimmedPath,
+            queryStringObject,
+            method,
+            headers,
+            'payload': buffer
+        }
+        //Route the request to the handler specified in the router 
+        chosenHandler(data, function (statusCode, payload) {
+            // Use the status code called back by the handler, or default to 200
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200
+            //Use the payload called back by the handler or default to {}
+            payload = typeof (payload) == 'object' ? payload : {}
+
+            // Convert the payload to a string 
+            var payloadString = JSON.stringify(payload) //payload send back by the handler to the user
+
+            //Send the response
+            res.setHeader('Content-Type', 'application/json')
+            res.writeHead(statusCode)
+            res.end(payloadString)
+            console.log('Returning this response :', statusCode, payload)
+        })
+    })
+
+}
+//Define the handlers
+var handlers = {}
+
+    //Sample handler
+    handlers.sample = function (data, callback) {
+        //Callback a http status code, and a payload object
+        callback(406, { 'name': 'sample handler' })
+    }
+
+    //Not found handler
+    handlers.notFound = function (data, callback) {
+        callback(404)
+    }
+    //Define a request router
+    var router = {
+        'sample': handlers.sample
+    }
